e3fb7dfe69da7f427de838d97ddd017cc98d3f74
Elden Li <lielden@yahoo.com>
2019-03-03 17:31:45 -0800

    part 2

--- Snapchat/Controller/Collection View/ImagePickerViewController.swift
+++ Snapchat/Controller/Collection View/ImagePickerViewController.swift
@@ -8,23 +8,35 @@
 
 import UIKit
 
-class ImagePickerViewController: UIViewController {
-
+class ImagePickerViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePickerCollectionView.delegate = self
+        imagePickerCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
+
+    @IBOutlet weak var imagePickerCollectionView: UICollectionView!
+    let data = Data()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.images.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "snapchatCollectionCell", for: indexPath) as? ImagePickerViewCell {
+            cell.collectionImage.image = UIImage(named: data.images[indexPath.item])
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected \(data.images[indexPath.row])")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let dimension = self.imagePickerCollectionView.frame.size.width / 4;
+        return CGSize(width: dimension, height: dimension)
+    }
 }
