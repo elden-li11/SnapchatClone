c6b6bf7f4703ad146e2ee6d79f65bbe4c527d1f5
Arman <arman.vaziri1@gmail.com>
2019-03-03 22:44:14 -0800

    updated constraints

---
+++ Snapchat/Controller/Table View/FeedPickerViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedPickerViewCell.swift
+//  Snapchat
+//
+//  Created by Arman Vaziri on 3/3/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedPickerViewCell: UITableView {
+    
+    
+    
+}
