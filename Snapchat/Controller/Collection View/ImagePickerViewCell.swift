a988d878589e029b4d76f0441729e0a3ef39562a
Elden Li <lielden@yahoo.com>
2019-03-03 16:45:28 -0800

    confusion

---
+++ Snapchat/Controller/Collection View/ImagePickerViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagePickerViewCell.swift
+//  Snapchat
+//
+//  Created by Elden Li on 3/3/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerViewCell: UICollectionViewCell {
+    @IBOutlet weak var collectionImage: UIImageView!
+}
---
+++ Snapchat/Controller/Collection View/ImagePickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImagePickerViewController.swift
+//  Snapchat
+//
+//  Created by Elden Li on 3/3/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
